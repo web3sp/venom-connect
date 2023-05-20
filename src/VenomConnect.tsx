367d02e20d26dd5f3aac206d7f27b9482dc1cbd2
Andrew <andrew@navy.online>
2023-05-20 13:10:23 +0300

    Update VenomConnect.tsx
    better check;

--- src/VenomConnect.tsx
+++ src/VenomConnect.tsx
@@ -84,10 +84,9 @@ class VenomConnect {
     const theme = options.theme || defaultOptions.theme;
     this.themeConfig = getThemeConfig(theme);
 
-    // Local Node network id is 0, so we need an additional isArray check in order to use it
+    // Local Node network id is 0, so we need an additional undefined check in order to use it
     const checkNetworkId =
-      (options.checkNetworkId || !Array.isArray(options.checkNetworkId)) ? options.checkNetworkId! : defaultOptions.checkNetworkId;
-    this.checkNetworkId = checkNetworkId;
+      options.checkNetworkId == undefined ? defaultOptions.checkNetworkId : options.checkNetworkId;
     this.checkNetworkId = checkNetworkId;
 
     const checkNetworkName =
